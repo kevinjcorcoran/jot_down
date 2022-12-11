0ade5b4c55c7aab84d9c60de91759c11747b5ab1
NafeeJ <nafeejan2@gmail.com>
2022-12-10 23:58:26 -0500

    Implement BottomSheet for entry editing

--- lib/widgets/entry_widget.dart
+++ lib/widgets/entry_widget.dart
@@ -33,7 +33,7 @@ class EntryWidget extends StatelessWidget {
         ),
         title: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Padding(
-            padding: const EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
             child: HashTagText(
               text: entry.content,
               decoratedStyle: const TextStyle(fontSize: 20, color: Colors.blue),
@@ -46,49 +46,77 @@ class EntryWidget extends StatelessWidget {
               },
             ),
           ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                  child: Row(children: [
-                IconButton(
-                    padding: const EdgeInsets.only(right: 5),
-                    constraints: const BoxConstraints(),
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      vm.editEntry(
-                          entry: entry,
-                          content: entry.content,
-                          time: entry.time,
-                          trash: true);
-                      updateView!();
-
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: const Text('Moved Entry to Trash'),
-                        action: SnackBarAction(
-                          label: 'UNDO',
-                          onPressed: () {
-                            vm.editEntry(
-                                entry: entry,
-                                content: entry.content,
-                                time: entry.time,
-                                trash: false);
-                            updateView!();
-                          },
-                        ),
-                      ));
-                    }),
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: const Icon(Icons.edit),
-                  onPressed: () {
-                    // TODO: Implement an new Edit Widget
-                  },
-                ),
-              ])),
-              // EditButton
-              Text(DateFormat.yMMMd().add_jm().format(entry.time))
-            ],
+          Padding(
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: Text(DateFormat.yMMMd().add_jm().format(entry.time)),
           )
-        ]));
+        ]),
+        onTap: () => showModalBottomSheet(
+            isScrollControlled: true,
+            backgroundColor: Colors.transparent,
+            context: context,
+            builder: (BuildContext context) => buildEntryEditSheet(context)
+        )
+    );
   }
+
+  Widget makeDismissible({required Widget child, required BuildContext context}) => GestureDetector(
+    behavior: HitTestBehavior.opaque,
+    onTap: () => Navigator.of(context).pop(),
+    child: GestureDetector(child: child)
+  );
+
+  Widget buildEntryEditSheet(BuildContext context) => makeDismissible(
+      child: DraggableScrollableSheet(
+        initialChildSize: 0.6,
+        minChildSize: 0.6,
+        maxChildSize: 0.8,
+        builder: (_, controller) => Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(10)
+              )
+          ),
+          padding: const EdgeInsets.all(15),
+          child: entryEditSheet(context, controller)
+        )
+    ),
+    context: context);
+  
+  Widget entryEditSheet(BuildContext context, ScrollController controller) => ListView(
+    controller: controller,
+    children: [
+      ElevatedButton.icon(
+        icon: const Icon(Icons.delete),
+        label: const Text("Move to Trash"),
+        onPressed: () {
+          vm.editEntry(
+              entry: entry,
+              content: entry.content,
+              time: entry.time,
+              trash: true);
+          updateView!();
+
+          Navigator.pop(context);
+
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: const Text('Moved Entry to Trash'),
+            action: SnackBarAction(
+              label: 'UNDO',
+              onPressed: () {
+                vm.editEntry(
+                    entry: entry,
+                    content: entry.content,
+                    time: entry.time,
+                    trash: false);
+                updateView!();
+              },
+            ),
+          ));
+        }
+      ),
+    ],
+  );
+
 }

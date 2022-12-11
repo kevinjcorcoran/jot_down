b304e4a2458a2801b150fb4b59d60e584c9b02c0
NafeeJ <nafeejan2@gmail.com>
2022-12-11 04:38:33 -0500

    Implement edit entry content

--- lib/widgets/entry_widget.dart
+++ lib/widgets/entry_widget.dart
@@ -10,7 +10,10 @@ class EntryWidget extends StatelessWidget {
   // The view model shared by the app
   final EntryListViewModel vm;
   // Updates the view model when changes are made
-  final Function({String title, String keyword})? updateView;
+  final Function({String title, String keyword, bool trash})? updateView;
+
+  final TextEditingController textEditingController = TextEditingController();
+
 
   /// Signifies if search widget is open, if so, pop the context on tag click to
   /// close the search
@@ -51,12 +54,21 @@ class EntryWidget extends StatelessWidget {
             child: Text(DateFormat.yMMMd().add_jm().format(entry.time)),
           )
         ]),
-        onTap: () => showModalBottomSheet(
-            isScrollControlled: true,
-            backgroundColor: Colors.transparent,
-            context: context,
-            builder: (BuildContext context) => buildEntryEditSheet(context)
-        )
+        onTap: () {
+          textEditingController.value = TextEditingValue(
+            text: entry.content,
+            selection: TextSelection.fromPosition(
+              TextPosition(offset: entry.content.length),
+            )
+          );
+
+          showModalBottomSheet(
+              isScrollControlled: true,
+              backgroundColor: Colors.transparent,
+              context: context,
+              builder: (BuildContext context) => buildEntryEditSheet(context)
+          );
+        }
     );
   }
 
@@ -71,7 +83,7 @@ class EntryWidget extends StatelessWidget {
         initialChildSize: 0.6,
         minChildSize: 0.6,
         maxChildSize: 0.8,
-        builder: (_, controller) => Container(
+        builder: (_, scrollController) => Container(
           decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.vertical(
@@ -79,44 +91,106 @@ class EntryWidget extends StatelessWidget {
               )
           ),
           padding: const EdgeInsets.all(15),
-          child: entryEditSheet(context, controller)
+          child: entryEditSheet(context, scrollController)
         )
     ),
     context: context);
   
-  Widget entryEditSheet(BuildContext context, ScrollController controller) => ListView(
-    controller: controller,
+  Widget entryEditSheet(BuildContext context, ScrollController scrollController) => ListView(
+    controller: scrollController,
     children: [
-      ElevatedButton.icon(
-        icon: const Icon(Icons.delete),
-        label: const Text("Move to Trash"),
+      sheetHandle(),
+      doneEditingButton(context),
+      editEntryTextField(),
+      moveToTrashButton(context),
+    ],
+  );
+
+  Widget sheetHandle() => FractionallySizedBox(
+    widthFactor: 0.1,
+    child: Container(
+      height: 5.0,
+      decoration: const BoxDecoration(
+        color: Colors.grey,
+        borderRadius:  BorderRadius.all(Radius.circular(2.5)),
+      ),
+    ),
+  );
+
+  Widget doneEditingButton(BuildContext context) => Align(
+      alignment: Alignment.topRight,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(fontSize: 20),
+        ),
         onPressed: () {
           vm.editEntry(
               entry: entry,
+              content: textEditingController.text,
+              time: entry.time,
+              trash: false
+          );
+          updateView!();
+          Navigator.pop(context);
+        },
+        child: const Text('DONE'),
+      )
+
+  );
+
+  Widget editEntryTextField() => HashTagTextField(
+    controller: textEditingController,
+    scrollPadding: const EdgeInsets.all(5.0),
+    decoratedStyle: const TextStyle(fontSize: 20, color: Colors.blue),
+    basicStyle: const TextStyle(fontSize: 20, color: Colors.black),
+    keyboardType: TextInputType.multiline,
+    minLines: 1,
+    maxLines: 10,
+    decoration: const InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide:
+          BorderSide(color: Color.fromARGB(255, 212, 212, 212), width: 1),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide:
+          BorderSide(color: Color.fromARGB(255, 212, 212, 212), width: 1),
+        ),
+        contentPadding: EdgeInsets.only(left: 10),
+        hintText: "Edit entry"
+    ),
+  );
+
+  Widget moveToTrashButton(BuildContext context) => ElevatedButton.icon(
+    icon: const Icon(Icons.delete),
+    label: const Text("Move to Trash"),
+    onPressed: () {
+      vm.editEntry(
+        entry: entry,
+        content: entry.content,
+        time: entry.time,
+        trash: true
+      );
+      updateView!();
+
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text('Moved Entry to Trash'),
+        action: SnackBarAction(
+          label: 'UNDO',
+          onPressed: () {
+            vm.editEntry(
+              entry: entry,
               content: entry.content,
               time: entry.time,
-              trash: true);
-          updateView!();
-
-          Navigator.pop(context);
-
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: const Text('Moved Entry to Trash'),
-            action: SnackBarAction(
-              label: 'UNDO',
-              onPressed: () {
-                vm.editEntry(
-                    entry: entry,
-                    content: entry.content,
-                    time: entry.time,
-                    trash: false);
-                updateView!();
-              },
-            ),
-          ));
-        }
-      ),
-    ],
+              trash: false);
+            updateView!();
+          },
+        ),
+      ));
+    }
   );
 
 }
